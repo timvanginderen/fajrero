87c62117087e3e506616bc14ad35e3ceebe0c79f
Tim Van Ginderen <timvanginderen@gmail.com>
2015-12-15 03:22:24 +0100

    Start basic MQTT server when starting main

--- app/src/main/java/be/tim/fajrero/MainActivity.java
+++ app/src/main/java/be/tim/fajrero/MainActivity.java
@@ -3,11 +3,22 @@ package be.tim.fajrero;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.eclipse.moquette.server.Server;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            new Server().startServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
