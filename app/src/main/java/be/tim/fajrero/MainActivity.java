63e49b53476ed4b7593e6181777f7b1f77a5624f
Tim Van Ginderen <timvanginderen@gmail.com>
2015-12-17 06:36:56 +0100

    Add open and close wifi access point options in main screen

--- app/src/main/java/be/tim/fajrero/MainActivity.java
+++ app/src/main/java/be/tim/fajrero/MainActivity.java
@@ -1,12 +1,16 @@
 package be.tim.fajrero;
 
 import android.content.Context;
+import android.net.wifi.WifiConfiguration;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import com.whitebyte.wifihotspotutils.WifiApManager;
 
 import org.eclipse.moquette.server.Server;
 import org.eclipse.paho.android.service.MqttAndroidClient;
@@ -32,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private MqttAndroidClient client;
+    private WifiApManager wifiApManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,36 +44,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        // Start mqtt server
-        try {
-            new Server().startServer();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Initialize wifi access point mananger
+        wifiApManager = new WifiApManager(this);
 
-        //create mqtt client
+        // Start mqtt server
+//        startMqttServer();
+
+        // Start mqtt client
+//        startMqttClient();
+
+    }
+
+    private void startMqttClient() {
+        createMqttClient();
+        connectMqttClient();
+    }
+
+    private void createMqttClient() {
         final String brokerUri = "tcp://0.0.0.0:1883";
         final String clientId = "Fajrero";
 
         client = createClient(this, brokerUri, clientId);
+    }
 
-        final String clientHandle = brokerUri + clientId;
-        final boolean ssl = false;
-        final int port = 1883;
-        final String host = "0.0.0.0";
-
-        Connection connection = new Connection(clientHandle, clientId, host, port, this, client, ssl);
-
-        connection.registerChangeListener(new PropertyChangeListener() {
-            @Override
-            public void propertyChange(PropertyChangeEvent event) {
-                Log.d(TAG, "propertyChange() called with: " + "event = [" + event + "]");
-            }
-        });
-
-        String[] actionArgs = new String[1];
-        actionArgs[0] = clientId;
-
+    private void connectMqttClient() {
         MqttConnectOptions conOpt = new MqttConnectOptions();
         conOpt.setCleanSession(true);
 
@@ -77,8 +76,6 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
                     Log.d(TAG, "onSuccess() called with: " + "asyncActionToken = [" + asyncActionToken + "]");
-
-//                    publishMessage(client);
                 }
 
                 @Override
@@ -91,17 +88,55 @@ public class MainActivity extends AppCompatActivity {
             Log.e(this.getClass().getCanonicalName(),
                     "MqttException Occured", e);
         }
-
     }
+
+    private void startMqttServer() {
+        try {
+            new Server().startServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     @OnClick(R.id.publish)
     public void publish(View view) {
         if (client == null) {
             return;
         }
-        publishMessage(client);
+        publishMessage();
     }
 
-    private void publishMessage(MqttAndroidClient client) {
+    @OnClick(R.id.open_access_point)
+    public void openAccessPoint(View view) {
+        WifiConfiguration config = getWifiConfiguration();
+        wifiApManager.setWifiApEnabled(config, true);
+    }
+    @OnClick(R.id.close_access_point)
+    public void closeAccessPoint(View view) {
+        wifiApManager.setWifiApEnabled(null, false);
+    }
+
+    @NonNull
+    private WifiConfiguration getWifiConfiguration() {
+        WifiConfiguration config = new WifiConfiguration();
+        config.SSID = "Waldo's mama";
+        config.preSharedKey  = "p@ssw0rd";
+//			config.hiddenSSID = true;
+        config.status = WifiConfiguration.Status.ENABLED;
+        config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+        config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+        config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+        config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+        config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+        config.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+        return config;
+    }
+
+    /**
+     * Publish a message on the mqtt broker
+     */
+    private void publishMessage() {
         MqttMessage message = new MqttMessage("Hello, I am Android Mqtt Client.".getBytes());
         message.setQos(2);
         message.setRetained(false);
@@ -122,8 +157,7 @@ public class MainActivity extends AppCompatActivity {
      * @param clientId The clientId for this client
      * @return new instance of MqttAndroidClient
      */
-    private MqttAndroidClient createClient(Context context, String serverURI, String clientId)
-    {
+    private MqttAndroidClient createClient(Context context, String serverURI, String clientId) {
         MqttAndroidClient client = new MqttAndroidClient(context, serverURI, clientId);
         return client;
     }
